075bdaa78bbbc91faa19f4cbb451683b6043923c
ennllzee <nattha_c@cmu.ac.th>
2021-09-21 18:38:48 +0700

    bottom

--- src/components/BottomBar/BottomBar.tsx
+++ src/components/BottomBar/BottomBar.tsx
@@ -1,7 +1,7 @@
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import { Grid, IconButton } from "@material-ui/core";
+import { Grid, IconButton, Typography } from "@material-ui/core";
 import { Business, Event, Help, History, Person } from "@material-ui/icons";
 import { history } from "../../helper/history";
 
@@ -11,7 +11,7 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1,
     },
     icon: {
-      marginRight: theme.spacing(2),
+      padding: 0,
     },
     title: {
       flexGrow: 1,
@@ -50,20 +50,38 @@ function BottomBar({ page }: BottomBarProps) {
             <IconButton
               color="inherit"
               onClick={() => history.push(`/profile&=${accessToken}`)}
+              className={classes.icon}
             >
-              <Person />
+              <Typography>
+                <Person />
+                {page === "Profile" && (
+                  <Typography style={{ fontSize: 8 }}>profile</Typography>
+                )}
+              </Typography>
             </IconButton>
             <IconButton
               color="inherit"
               onClick={() => history.push(`/appointment&=${accessToken}`)}
+              className={classes.icon}
             >
-              <Event />
+              <Typography>
+                <Event />
+                {page === "Appoinment" && (
+                  <Typography style={{ fontSize: 8 }}>appointment</Typography>
+                )}
+              </Typography>
             </IconButton>
             <IconButton
               color="inherit"
               onClick={() => history.push(`/history&=${accessToken}`)}
+              className={classes.icon}
             >
-              <History />
+              <Typography>
+                <History />
+                {page === "History" && (
+                  <Typography style={{ fontSize: 8 }}>history</Typography>
+                )}
+              </Typography>
             </IconButton>
             {/* <IconButton
               color="inherit"
